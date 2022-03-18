94c8d882734e02b520c9f65f2d28b81573e810bc
p-laranjinha <plcasimiro@ua.pt>
2022-03-18 17:32:15 +0000

    intellij project start

---
+++ PA1C2G0/src/CC/Main.java
@@ -0,0 +1,9 @@
+package CC;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ PA1C2G0/src/HC/Main.java
@@ -0,0 +1,9 @@
+package HC;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
