6c995272dcd02c93b301bc37795d04b2ed4c3536
p-laranjinha <plcasimiro@ua.pt>
2022-03-18 23:55:21 +0000

    ccp gui start

---
+++ PA1C2G0/src/CC/GUI.java
@@ -0,0 +1,62 @@
+package CC;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI {
+    private JPanel mainPanel;
+    private JLabel statusLabel;
+    private JButton startButton;
+    private JButton suspendButton;
+    private JButton resumeButton;
+    private JButton stopButton;
+    private JButton endButton;
+    private JComboBox operatingModeComboBox;
+    private JButton allowPatientButton;
+    private JPanel cardPanel;
+    private JSpinner adultPatientsSpinner;
+    private JSpinner childrenPatientsSpinner;
+    private JSpinner seatsSpinner;
+    private JComboBox moveTimeSpinner;
+    private JComboBox evaluationTimeComboBox;
+    private JComboBox appointmentTimeSpinner;
+    private JComboBox paymentTimeSpinner;
+    private JButton cancelFormButton;
+    private JButton startFormButton;
+
+    public GUI() {
+        adultPatientsSpinner.setModel(new SpinnerNumberModel(10, 1, 50, 1));
+        childrenPatientsSpinner.setModel(new SpinnerNumberModel(10, 1, 50, 1));
+        seatsSpinner.setModel(new SpinnerNumberModel(4, 2, 10, 2));
+        suspendButton.setEnabled(false);
+        resumeButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        allowPatientButton.setEnabled(false);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ((CardLayout) cardPanel.getLayout()).next(cardPanel);
+            }
+        });
+        startFormButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // startButton.setEnabled(false);
+                ((CardLayout) cardPanel.getLayout()).next(cardPanel);
+            }
+        });
+        cancelFormButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ((CardLayout) cardPanel.getLayout()).next(cardPanel);
+            }
+        });
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
--- PA1C2G0/src/CC/Main.java
+++ PA1C2G0/src/CC/Main.java
@@ -1,9 +1,36 @@
 package CC;
 
+import javax.swing.*;
+import javax.swing.UIManager.LookAndFeelInfo;
+import java.awt.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static void changeGUILook(String wantedLook) {
+        LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels();
+        String chosenLook = null;
+        for (LookAndFeelInfo look: looks)
+            if (wantedLook.equals(look.getName()))
+                chosenLook = look.getClassName();
+        if (chosenLook == null)
+            chosenLook = UIManager.getSystemLookAndFeelClassName();
+        try {
+            UIManager.setLookAndFeel(chosenLook);
+            JFrame.setDefaultLookAndFeelDecorated(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void main(String[] args) {
+        changeGUILook("GTK+");
+        JFrame frame = new JFrame("CCP");
+        frame.setContentPane(new GUI().getMainPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(380, 310));
+        frame.setPreferredSize(new Dimension(400, 310));
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
