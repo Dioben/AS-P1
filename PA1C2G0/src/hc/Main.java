235620871128464b95f99d244104f190d2a431ae
p-laranjinha <plcasimiro@ua.pt>
2022-04-16 15:35:28 +0100

    gui caching + secondary theme

--- PA1C2G0/src/hc/Main.java
+++ PA1C2G0/src/hc/Main.java
@@ -1,11 +1,9 @@
 package hc;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        GUI.setGUILook("GTK+");
+    public static void main(String[] args) {
+        GUI.setGUILook(new String[] {"GTK+", "Nimbus"});
         GUI gui = new GUI();
         gui.start();
     }
