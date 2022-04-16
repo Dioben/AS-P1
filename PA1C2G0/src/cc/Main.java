2cf48fb630c9671e01a8248b04e0ddd680704457
buckaroo69 <diogobento@ua.pt>
2022-04-16 15:10:53 +0100

    update docs for CCP

--- PA1C2G0/src/cc/Main.java
+++ PA1C2G0/src/cc/Main.java
@@ -2,7 +2,8 @@ package cc;
 
 public class Main {
     /**
-     * Launches a client pointed towards localhost 8000 and a UI process
+     * Launches a client instance with a GUI
+     * Port and host to connect to must be established during runtime
      * @param args Command line arguments, unused
      */
     public static void main(String[] args) throws InterruptedException {
